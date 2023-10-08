69762922641f6d3789730abc9457475ae14fabf7
Isaac <isaacmaffeis@gmail.com>
2023-10-08 15:40:10 +0200

    added source code of exe2

---
+++ exe2/exe2.2_albero_binario/src/alberoBinario/NodoBinario.java
@@ -0,0 +1,16 @@
+//Nodo di un albero binario
+package alberoBinario;
+
+
+class NodoBinario{
+	public Object elem;
+	public NodoBinario padre;
+	public NodoBinario sinistro;
+	public NodoBinario destro;
+	
+
+	public NodoBinario(Object el){
+		elem = el;
+		padre = sinistro= destro = null;	
+	}
+}
