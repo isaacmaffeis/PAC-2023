9be4cacd82d4f927775453f8991ae7f7fbe489dc
isaacmaffo96 <isaacmaffeis@gmail.com>
2023-12-01 20:49:52 +0100

    sax-books

--- tut4/XML_SAX-Libro/src/Main.java
+++ tut4/XML_SAX-Libro/src/Main.java
@@ -1,9 +1,40 @@
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.SAXException;
+
 public class Main {
+
+	public static void lookupDocument(String path) {
+		SaxParser h = new SaxParser();
+
+		SAXParserFactory spf = SAXParserFactory.newInstance();
+		spf.setValidating(true);
+		spf.setNamespaceAware(false);
+
+		try {
+			SAXParser sp = spf.newSAXParser();
+			sp.parse(path, h);
+			h.printBooks();
+			if (h.hadProblems()) {
+				System.out.println("Il documento contiene errori.");
+			}
+		} catch (ParserConfigurationException ex) {
+			ex.printStackTrace();
+			System.exit(10);
+		} catch (SAXException sxe) {
+			System.out.println("Il documento contiene errori fatali!");
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+	}
+
 	public static void main(String[] args) {
-    
-		SaxParser p = new SaxParser();
-		p.parseDocument("libro.xml");
-		p.printBooks();
-		
-  }
+
+		lookupDocument("libri.xml");
+
+	}
 }
